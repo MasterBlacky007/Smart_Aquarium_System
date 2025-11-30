f18ac47dc9ee652f8ec4ed71c23ba43412104a7e
shasani <91341553+MasterBlacky007@users.noreply.github.com>
2025-11-30 16:59:23 +0530

    complete final

--- test03/test03.ino
+++ test03/test03.ino
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <ESP32Servo.h>
@@ -25,10 +26,21 @@ FirebaseConfig config;
 #define ONE_WIRE_BUS 4
 #define HEATER_RELAY 21
 #define COOLING_RELAY 25
+=======
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Firebase_ESP_Client.h>
+
+
+#define ONE_WIRE_BUS 4
+#define HEATER_RELAY 25
+#define COOLING_RELAY 21
+>>>>>>> 09619f24fb4fefc0c78ca6bece8108f59996a6bc
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+<<<<<<< HEAD
 float waterTemp = 0.0;
 const float MIN_TEMP = 29.0;
 const float MAX_TEMP = 30.0;
@@ -337,4 +349,67 @@ void loop() {
   }
 
   delay(10);
+=======
+extern FirebaseData fbdo;   
+extern bool fbSetFloat(const char* path, float v);
+extern bool fbSetBool(const char* path, bool v);
+
+float getWaterTemperature() {
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+
+  if (tempC == DEVICE_DISCONNECTED_C || tempC < -20 || tempC > 80) {
+    Serial.println("Error: Temperature sensor not detected!");
+    return -127.0;
+  }
+
+  return tempC;
+}
+
+void updateHeaterCooler(float tempC) {
+  bool heaterOn = false;
+  bool coolerOn = false;
+
+  if (tempC < 29.0) {
+    heaterOn = true;
+    coolerOn = false;
+  } 
+  else if (tempC > 30.0) {
+    heaterOn = false;
+    coolerOn = true;
+  } 
+  else {
+    heaterOn = false;
+    coolerOn = false;
+  }
+
+  digitalWrite(HEATER_RELAY, heaterOn);
+  digitalWrite(COOLING_RELAY, coolerOn);
+
+  // Firebase update
+  fbSetBool("/control/heater", heaterOn);
+  fbSetBool("/control/cooler", coolerOn);
+
+  Serial.print("Heater: ");
+  Serial.print(heaterOn ? "ON" : "OFF");
+  Serial.print("  |  Cooler: ");
+  Serial.println(coolerOn ? "ON" : "OFF");
+}
+
+void updateTemperatureFirebase(float tempC) {
+  fbSetFloat("/sensor/temperature", tempC);
+}
+
+void handleTemperatureSystem() {
+  float temp = getWaterTemperature();
+
+  if (temp != -127.0) {
+    Serial.print("Water Temperature: ");
+    Serial.print(temp);
+    Serial.println(" °C");
+
+    updateTemperatureFirebase(temp);
+    updateHeaterCooler(temp);
+  }
+>>>>>>> 09619f24fb4fefc0c78ca6bece8108f59996a6bc
 }
